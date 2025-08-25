c4b99fd5a17a811698d44d5327a96f82da3fd0d8
pedro ribeiro <161263454+pedroribeiro02@users.noreply.github.com>
2025-08-25 10:28:19 -0300

    Update index.php

--- app2/index.php
+++ app2/index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Fale Conosco</h1>
 
+    <form method="post" action="">
+        <label for="nome">Nome:</label><br>
+        <input type="text" name="nome" id="nome" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" name="email" id="email" required><br><br>
+    </form>
+</body>
+</html>
