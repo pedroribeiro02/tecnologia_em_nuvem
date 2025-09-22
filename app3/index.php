605235b533b23ec2891a8518e56a1d2c7fff4100
pedro ribeiro <161263454+pedroribeiro02@users.noreply.github.com>
2025-09-22 10:11:18 -0300

    Update index.php

--- app3/index.php
+++ app3/index.php
@@ -9,7 +9,7 @@ function stringParaBinario($string) {
   $binario = '';
   foreach(str_split($string) as $char) {
     // Converte o caractere para o valor binário de 8 bits
-  $binario ,= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . '';
+  $binario ,= str_pad(decbin(ord($char)) 8, '0', STR_PAD_LEFT) . '';
   }
   return $binario
     }
